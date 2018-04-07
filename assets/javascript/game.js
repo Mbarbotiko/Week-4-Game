6099bac5d496f74db6d2bf14cd1dfbe8e83b5ac3
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-07 08:36:03 -0500

    not pretty but it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,27 +3,28 @@ $(document).ready(function () {
 
     var losses = 0;
     var wins = 0;
-    var crystalValue = [];
+    var crystalValue = [0];
     var blueRandom;
     var greenRandom;
     var pinkRandom;
     var purpleRandom;
     var randomNumber;
-    //math function chooses a number between 1-12
+   
 
     function randomNumber() {
         randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+         //math function chooses a number between 19 and 120
         blueRandom = 1;//throwing you a frickin' bone
         greenRandom = Math.floor(Math.random() * 12) + 1;
         pinkRandom = Math.floor(Math.random() * 12) + 1;
         purpleRandom = Math.floor(Math.random() * 12) + 1;
+         //math function chooses a number between 1-12
         $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
         //adds the random number generated to the HTML
     }
 
     randomNumber();
 
-    //math function chooses a number between 19 and 120
 
 
     //$("#random-number").html("<h2>Match this number:<h2> "+randomNumber);
@@ -59,24 +60,41 @@ $(document).ready(function () {
     $(".crystals").click(function () {
         totalGuess = crystalValue.reduce((total, amount) => total + amount);
         $("#score").text(totalGuess);
-
-        if (totalGuess === randomNumber) {
-            wins++;
-            $("#wins").text("Wins: " + wins);
-            crystalValue = [];
-
-
-        }
-
-        else if (totalGuess > randomNumber) {
-            losses++;
-            $("#losses").text("Losses: " + losses);
-            crystalValue = [];
-
-        }
-
-
+        endGame();
+        //all pushed numbers in the array crystal value are added together
     });
+
+    function endGame(){
+    if (totalGuess === randomNumber) {
+         wins++;
+        $("#wins").text("Wins: " + wins);
+        crystalValue = [0];
+        randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+        $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
+        greenRandom = Math.floor(Math.random() * 12) + 1;
+        pinkRandom = Math.floor(Math.random() * 12) + 1;
+        purpleRandom = Math.floor(Math.random() * 12) + 1;
+
+
+    }
+
+    else if (totalGuess > randomNumber) {
+        losses++;
+         $("#losses").text("Losses: " + losses);
+         crystalValue = [0];
+         randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+         $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
+         greenRandom = Math.floor(Math.random() * 12) + 1;
+        pinkRandom = Math.floor(Math.random() * 12) + 1;
+        purpleRandom = Math.floor(Math.random() * 12) + 1;
+
+    }
+    }
+
+    
+
+
+    
 
 
 
