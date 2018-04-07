a4f062c4dcfd2b4a574eda4cd6eba652fa96c6d9
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-06 22:05:45 -0500

    reset guess number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,70 +4,88 @@ $(document).ready(function () {
     var losses = 0;
     var wins = 0;
     var crystalValue = [];
-    
-        
+    var blueRandom;
+    var greenRandom;
+    var pinkRandom;
+    var purpleRandom;
+    var randomNumber;
+    //math function chooses a number between 1-12
 
-
-
-
-        var blueRandom = 1;
-        var greenRandom = Math.floor(Math.random() * 12) + 1;
-        var pinkRandom = Math.floor(Math.random() * 12) + 1;
-        var purpleRandom = Math.floor(Math.random() * 12) + 1;
-        //math function chooses a number between 1-12
-        var randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-        //math function chooses a number between 19 and 120
-
-
-        $("#random-number").text("Match this number: "+randomNumber);
+    function randomNumber() {
+        randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+        blueRandom = 1;//throwing you a frickin' bone
+        greenRandom = Math.floor(Math.random() * 12) + 1;
+        pinkRandom = Math.floor(Math.random() * 12) + 1;
+        purpleRandom = Math.floor(Math.random() * 12) + 1;
+        $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
         //adds the random number generated to the HTML
+    }
 
-        $("#blue").click(function () {
-            crystalValue.push(blueRandom);
-            //pushes the random number generated to the array crystalvalue upon a click
+    randomNumber();
 
-
-        });
-
-        $("#green").click(function () {
-            crystalValue.push(greenRandom);
-
-        });
-
-        $("#pink").click(function () {
-            crystalValue.push(pinkRandom);
+    //math function chooses a number between 19 and 120
 
 
-        });
+    //$("#random-number").html("<h2>Match this number:<h2> "+randomNumber);
+    //adds the random number generated to the HTML
 
-        $("#purple").click(function () {
-            crystalValue.push(purpleRandom);
+    $("#blue").click(function () {
+        crystalValue.push(blueRandom);
+        //pushes the random number generated to the array crystalvalue upon a click
+        console.log(crystalValue);
+
+    });
+
+    $("#green").click(function () {
+        crystalValue.push(greenRandom);
+        console.log(crystalValue);
+    });
+
+    $("#pink").click(function () {
+        crystalValue.push(pinkRandom);
+        console.log(crystalValue);
+
+    });
+
+    $("#purple").click(function () {
+        crystalValue.push(purpleRandom);
+        console.log(crystalValue);
+
+
+    });
 
 
 
-        });
+    $(".crystals").click(function () {
+        totalGuess = crystalValue.reduce((total, amount) => total + amount);
+        $("#score").text(totalGuess);
 
-        $(".crystals").click(function () {
-            totalGuess = crystalValue.reduce((total, amount) => total + amount);
-            $("#score").text(totalGuess);
-            if (totalGuess === randomNumber) {
-                wins++;
-                $("#wins").text("Wins: " + wins);
-                document;
-                
+        if (totalGuess === randomNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            crystalValue = [];
 
-            }
 
-            else if (totalGuess > randomNumber) {
-                losses++;
-                $("#losses").text("Losses: " + losses);
-                document;
-                
-            }
-            //adds all the numbers in array together and stores the sum into a new variable called totalguess, doesnt execute the function until one of the crystals is clicked otherwise errors out the game immediately.
+        }
 
-        });
+        else if (totalGuess > randomNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            crystalValue = [];
 
-    
+        }
+
+
+    });
+
+
+
+
+
+    //adds all the numbers in array together and stores the sum into a new variable called totalguess, doesnt execute the function until one of the crystals is clicked otherwise errors out the game immediately.
+
+
+
+
 
 });
