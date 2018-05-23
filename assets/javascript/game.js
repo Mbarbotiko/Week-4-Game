1964130d3f03cb824c90000f7f7ba34168053fb1
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-05-23 12:55:33 -0500

    adding css and sounds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,20 @@ $(document).ready(function () {
 
     var soundChime = new Audio();
     soundChime.src = "assets/music/chime.mp3";
+    soundChime.playbackRate = .5;
+    var backgroundMusic = new Audio();
+    backgroundMusic.src = "assets/music/04 Emerald Hill Zone.mp3"
+    var winSound = new Audio();
+    winSound.src = "assets/music/Ta-da.mp3"
+    winSound.volume = .5;
+    var loseSound = new Audio();
+    loseSound.src = "assets/music/Break.mp3"
+    $("#game-container").hide();
 
-    $("#instructions-container").on('click', function(){
+    $("#instructions-container").on('click', function () {
         $("#instructions-container").hide();
+        backgroundMusic.play();
+        $("#game-container").show();
     });
 
     var losses = 0;
@@ -17,9 +28,9 @@ $(document).ready(function () {
     var purpleRandom;
     var randomNumber;
     reset();
-   
+
     $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
-  
+
     function reset() {
         randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
         greenRandom = Math.floor(Math.random() * 12) + 1;
@@ -28,12 +39,13 @@ $(document).ready(function () {
         randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
     }
 
-  
+
     $("#blue").click(function () {
         crystalValue.push(blueRandom);
         $("#blue").fadeTo(500, 0.4)
         $("#blue").fadeTo(500, 1)
         soundChime.play();
+
     });
 
     $("#green").click(function () {
@@ -72,6 +84,7 @@ $(document).ready(function () {
             crystalValue = [0];
             reset();
             $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
+            winSound.play();
         }
 
         else if (totalGuess > randomNumber) {
@@ -80,6 +93,7 @@ $(document).ready(function () {
             crystalValue = [0];
             reset();
             $("#random-number").html("<h2>Match this number:<h2> " + randomNumber);
+            loseSound.play();
         }
     }
 
